c8f649c35b311c193780a50dfeaba8335cf8827e
bgpjh <bgpjh97@gmail.com>
2021-01-21 17:36:51 +0900

    'add_schedule_date'

--- app/src/main/java/com/cookandroid/calendar/ScheduleActivity.java
+++ app/src/main/java/com/cookandroid/calendar/ScheduleActivity.java
@@ -14,26 +14,76 @@ import android.widget.CompoundButton;
 import android.widget.LinearLayout;
 import android.widget.Switch;
 
-public class ScheduleActivity extends AppCompatActivity {
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
+public class ScheduleActivity extends AppCompatActivity {
+    String Year,Month,Day,Hour,Min;
 //    SharedPreferences setColor;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
   //      setColor = getSharedPreferences("settingColor",MODE_PRIVATE);
         SharedPreferences setColor = getSharedPreferences("settingColor",MODE_PRIVATE);
         SharedPreferences.Editor editor = setColor.edit();
+        final SharedPreferences setStartDate = getSharedPreferences("startTimer",MODE_PRIVATE);
+        SharedPreferences.Editor editor1 = setStartDate.edit();
+        final SharedPreferences setEndDate = getSharedPreferences("endTimer",MODE_PRIVATE);
+        SharedPreferences.Editor editor2 = setEndDate.edit();
         editor.putInt("scheduleColor", Color.parseColor("#000000"));
         editor.apply();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.schedulemain);
 
+        SimpleDateFormat dateFormat;
+        long mNow = System.currentTimeMillis();
+        Min = "00";
+        Date date = new Date(mNow);
+        dateFormat = new SimpleDateFormat("yyyy");
+        Year = dateFormat.format(date);
+        editor1.putString("Year",Year);
+        dateFormat = new SimpleDateFormat("MM");
+        Month = dateFormat.format(date);
+        editor1.putString("Month",Month);
+        dateFormat = new SimpleDateFormat("dd");
+        Day = dateFormat.format(date);
+        editor1.putString("Day",Day);
+        dateFormat = new SimpleDateFormat("HH");
+        Hour = dateFormat.format(date);
+        editor1.putString("Hour",Hour);
+        editor1.putString("Min",Min);
 
+        mNow += 3600000;
+        date = new Date(mNow);
+        dateFormat = new SimpleDateFormat("yyyy");
+        Year = dateFormat.format(date);
+        editor2.putString("Year",Year);
+        dateFormat = new SimpleDateFormat("MM");
+        Month = dateFormat.format(date);
+        editor2.putString("Month",Month);
+        dateFormat = new SimpleDateFormat("dd");
+        Day = dateFormat.format(date);
+        editor2.putString("Day",Day);
+        dateFormat = new SimpleDateFormat("HH");
+        Hour = dateFormat.format(date);
+        editor2.putString("Hour",Hour);
+        editor2.putString("Min",Min);
+
+        editor1.apply();
+        editor2.apply();
 
         final Button colorButton = (Button) findViewById(R.id.color);
+        final Button startDButton = (Button) findViewById(R.id.startDateButton);
+        final Button endDButton = (Button) findViewById(R.id.endDateButton);
         final Switch allDaySwitch = (Switch) findViewById(R.id.allDaySwitch);
         final LinearLayout startDayLayout = (LinearLayout) findViewById(R.id.start_Date_Layout);
         final LinearLayout endDayLayout = (LinearLayout) findViewById(R.id.end_Date_Layout);
         colorButton.setBackgroundColor(Color.BLACK);
+
+//        startDButton.setText(setStartDate.getString("Year","")+"/"+setStartDate.getString("Month",""+"")+"/"
+//                +setStartDate.getString("Day","")+" "+setStartDate.getString("Hour","")+":"+setStartDate.getString("Min",""));
+
+//        endDButton.setText(setEndDate.getString("Year","")+"/"+setEndDate.getString("Month",""+"")+"/"
+//                +setEndDate.getString("Day","")+" "+setEndDate.getString("Hour","")+":"+setEndDate.getString("Min",""));
 
         colorButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -41,6 +91,23 @@ public class ScheduleActivity extends AppCompatActivity {
                 colorDialog colorDlg = new colorDialog(ScheduleActivity.this);
                 colorDlg.callFunction(colorButton);
                 }
+        });
+        startDButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dateDialog stDlg = new dateDialog(ScheduleActivity.this);
+                stDlg.callFunction(startDButton,setStartDate.getString("Year",""),setStartDate.getString("Month",""),
+                        setStartDate.getString("Day",""),setStartDate.getString("Hour",""),setStartDate.getString("Min",""),0);
+            }
+        });
+
+        endDButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dateDialog stDlg = new dateDialog(ScheduleActivity.this);
+                stDlg.callFunction(endDButton,setEndDate.getString("Year",""),setEndDate.getString("Month",""),
+                        setEndDate.getString("Day",""),setEndDate.getString("Hour",""),setEndDate.getString("Min",""),1);
+            }
         });
 
         allDaySwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
---
+++ app/src/main/java/com/cookandroid/calendar/dateDialog.java
@@ -0,0 +1,106 @@
+package com.cookandroid.calendar;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
+
+import java.lang.reflect.Array;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+
+public class dateDialog extends AppCompatActivity {
+    private Context context;
+    SharedPreferences s_timer;
+    SharedPreferences.Editor editor;
+
+    ArrayList<String> arrayList;
+    ArrayAdapter<String> arrayAdapter;
+
+    Spinner yearSpinner;
+    Spinner monthSpinner;
+    Spinner daySpinner;
+
+    public dateDialog(Context context) {
+        this.context = context;
+/*
+        s_timer = context.getSharedPreferences("startTimer",MODE_PRIVATE);
+        editor = s_timer.edit();*/
+    }
+
+    public void callFunction(final Button button,String Y,String M,String D,String H,String Minute,int Type) {
+        final Dialog dlg = new Dialog(context);
+        String Year = Y;
+        String Month = M;
+        String Day = D;
+        String Hour = H;
+        String Min = Minute;
+
+        if(Type==0) s_timer = context.getSharedPreferences("startTimer",MODE_PRIVATE);
+        else s_timer = context.getSharedPreferences("endTimer",MODE_PRIVATE);
+        editor = s_timer.edit();
+        dlg.setContentView(R.layout.schedule_color);
+        dlg.show();
+
+        final Button cancel = (Button) dlg.findViewById(R.id.dateCancel);
+        final Button accept = (Button) dlg.findViewById(R.id.dateAccept);
+
+        yearSpinner = (Spinner) dlg.findViewById(R.id.YearSpinner);
+        monthSpinner = (Spinner) dlg.findViewById(R.id.MonthSpinner);
+        monthSpinner.setSelection(Integer.parseInt(Month));
+
+        setDaySpinnerValue(Integer.parseInt(Year));
+        //yearSpinner.setPrompt();
+
+        cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dlg.cancel();
+            }
+        });
+        accept.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dlg.cancel();
+            }
+        });
+    }
+
+    public void setDaySpinnerValue(int Year) {
+        int Position = monthSpinner.getSelectedItemPosition();
+        boolean leap = false;
+        if (Year%4==0&&(Year%100!=0||Year%400==0)) leap = true;
+        int dayNum=0;
+        switch(Position) {
+            case 2:
+                dayNum=28;
+                if (leap) dayNum=29;
+                break;
+            case 4:
+            case 6:
+            case 9:
+            case 11:
+                dayNum=30;
+                break;
+            case 1:
+            case 3:
+            case 5:
+            case 7:
+            case 10:
+            case 12:
+                dayNum=31;
+                break;
+        }
+        arrayList = new ArrayList<String>();
+        for (int i=1;i<=dayNum;i++) {
+            arrayList.add(String.valueOf(i));
+        }
+        arrayAdapter = new ArrayAdapter<String>(getApplicationContext(),android.R.layout.simple_spinner_dropdown_item,arrayList);
+        daySpinner.setAdapter(arrayAdapter);
+        daySpinner.setSelection(0);
+    }
+}
