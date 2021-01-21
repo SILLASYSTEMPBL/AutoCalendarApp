a9c4156e503cafbdbca120ce9682f46393e16c00
bgpjh <bgpjh97@gmail.com>
2021-01-21 21:59:24 +0900

    'id_conflict_remove'

--- app/src/main/java/com/cookandroid/calendar/ScheduleActivity.java
+++ app/src/main/java/com/cookandroid/calendar/ScheduleActivity.java
@@ -1,15 +1,12 @@
 package com.cookandroid.calendar;
 
-import android.content.Intent;
 import android.content.SharedPreferences;
 import android.database.sqlite.SQLiteDatabase;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.text.Layout;
 import android.view.View;
-import android.view.ViewConfiguration;
 import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.EditText;
