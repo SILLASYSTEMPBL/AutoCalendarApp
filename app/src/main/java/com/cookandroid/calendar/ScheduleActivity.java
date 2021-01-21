b91ebc19175b0f9d176bea1d703c740ebbe7dedd
bgpjh <bgpjh97@gmail.com>
2021-01-21 14:12:33 +0900

    'color_Button_stabilized'

--- app/src/main/java/com/cookandroid/calendar/ScheduleActivity.java
+++ app/src/main/java/com/cookandroid/calendar/ScheduleActivity.java
@@ -2,12 +2,16 @@ package com.cookandroid.calendar;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Layout;
 import android.view.View;
 import android.view.ViewConfiguration;
 import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.LinearLayout;
 import android.widget.Switch;
 
 public class ScheduleActivity extends AppCompatActivity {
@@ -16,6 +20,10 @@ public class ScheduleActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
   //      setColor = getSharedPreferences("settingColor",MODE_PRIVATE);
+        SharedPreferences setColor = getSharedPreferences("settingColor",MODE_PRIVATE);
+        SharedPreferences.Editor editor = setColor.edit();
+        editor.putInt("scheduleColor", Color.parseColor("#000000"));
+        editor.apply();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.schedulemain);
 
@@ -23,6 +31,9 @@ public class ScheduleActivity extends AppCompatActivity {
 
         final Button colorButton = (Button) findViewById(R.id.color);
         final Switch allDaySwitch = (Switch) findViewById(R.id.allDaySwitch);
+        final LinearLayout startDayLayout = (LinearLayout) findViewById(R.id.start_Date_Layout);
+        final LinearLayout endDayLayout = (LinearLayout) findViewById(R.id.end_Date_Layout);
+        colorButton.setBackgroundColor(Color.BLACK);
 
         colorButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -30,6 +41,21 @@ public class ScheduleActivity extends AppCompatActivity {
                 colorDialog colorDlg = new colorDialog(ScheduleActivity.this);
                 colorDlg.callFunction(colorButton);
                 }
+        });
+
+        allDaySwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked) {
+                    startDayLayout.setVisibility(View.INVISIBLE);
+                    //startDayLayout.setScaleY(0.01f);
+                    endDayLayout.setVisibility(View.INVISIBLE);
+                }
+                else {
+                    startDayLayout.setVisibility(View.VISIBLE);
+                    endDayLayout.setVisibility(View.VISIBLE);
+                }
+            }
         });
     }
 }
